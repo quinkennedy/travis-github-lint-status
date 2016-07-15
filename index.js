a0f2a47372445e5ef38225898266bc8fe77bc291
Quin Kennedy <quinkennedy@gmail.com>
2016-07-15 12:04:43 -0700

    running eslint and updating GH status

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 var GitHubAPI = require('github');
+var ESLint = require('eslint');
+
+var report = processFiles('.');
+printReport(report);
+updateStatus(report);
+
+function processFiles(glob){
+  var cli = new ESLint.CLIEngine();
+  var report = cli.executeOnFiles(cli.resolveFileGlobPatterns([glob]));
+  return report;
+}
+
+function printReport(report){
+  for (var result of report.results){
+    if (result.errorCount !== 0 ||
+        result.warningCount !== 0){
+      console.log(result.filePath);
+      for (var message of result){
+        var type = (message.severity == 2 ? 'error' : 'warng');
+        console.log(type, 'line', message.line, message.message, message.ruleId);
+      }
+    }
+  }
+}
+
+function updateStatus(report){
+
+  // authenticate with API
+  GitHubAPI.authenticate({
+    type:  'oauth',
+    token: process.env.TRAVIS_GITHUB_LINT_STATUS_TOKEN
+  });
+
+  // get travis environment variables
+  var travisSlug = process.env.TRAVIS_REPO_SLUG;
+  var travisJob = process.env.TRAVIS_JOB_ID; 
+  var travisEvent = process.env.TRAVIS_EVENT_TYPE;
+  var sha = process.env.TRAVIS_COMMIT;
+
+  // parse "slug" into name/repo segments for github module
+  var parsedSlug = travisSlug.split('/');
+  var user = parsedSlug[0];
+  var repo = parsedSlug[1];
+
+  // create other parameters for github module
+  var target_url = 'https://travis-ci.org/'+travisSlug+'/jobs/'+travisJob;
+  var state = (report.errorCount === 0 ? 'success' : 'failure');
+  var description = 'errors: ' + report.errorCount + 
+                    ', warnings: ' + report.warningCount;
+  var context = 'ci/lint/'+travisEvent;
+  
+  // make API call
+  GitHubAPI.repos.createStatus({
+    user,
+    repo,
+    sha,
+    state,
+    target_url,
+    description,
+    context
+  });
+}
+
