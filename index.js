0ac9a673a584f2d092195a776b0993664a743b1e
Quin Kennedy <quinkennedy@gmail.com>
2016-07-15 13:27:00 -0700

    added comments and reduced complexity

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+// used for interfacing with the GitHub API
 var GitHubAPI = require('github');
+// the actual Linter
 var ESLint = require('eslint');
+// used for colorizing the console output to increase readability
 var Colors = require('colors/safe');
 
+// run ESLint on all project files
 var report = processFiles('.');
+// print the results to the console
 printReport(report);
+// notify GitHub of the status
 updateStatus(report);
 
+/**
+ *  runs ESLint on all the project files
+ *  @param {string} glob the pattern to use for file discovery
+ *  @returns {object} ESLint report
+ */
 function processFiles(glob){
   var cli = new ESLint.CLIEngine();
   var report = cli.executeOnFiles(cli.resolveFileGlobPatterns([glob]));
   return report;
 }
 
+/**
+ *  Print the ESLint report to the console
+ *  @param {object} report The ESLint report to print
+ */
 function printReport(report){
 
   console.log(Colors.bold('-- begin ESLint report --\n'));
@@ -24,31 +39,55 @@ function printReport(report){
     if (result.errorCount !== 0 ||
         result.warningCount !== 0){
 
-      console.log(Colors.underline(result.filePath));
+      printFileResult(result);
 
-      for (var message of result.messages){
-        var type, typeColor;
-
-        if (message.severity === 2){
-          // pad 'error' string to match 'warning' string length
-          type = 'error  ';
-          typeColor = Colors.red;
-        } else {
-          type = 'warning';
-          typeColor = Colors.yellow;
-        }
-
-        console.log('  ',
-                    typeColor(type),
-                    Colors.gray(message.line),
-                    message.message,
-                    Colors.gray(message.ruleId));
-      }
-
-      // extra line at the end to provide visual separation between files
-      console.log();
     }
   }
+
+  printReportFooter(report);
+}
+
+/**
+ * Prints the results of the specific file
+ * @param {object} result The ESLint results for a single file
+ */
+function printFileResult(result){
+
+  console.log(Colors.underline(result.filePath));
+
+  if (result.messages.length === 0){
+    console.log('no issues');
+
+  } else {
+    for (var message of result.messages){
+      var type, typeColor;
+  
+      if (message.severity === 2){
+        // pad 'error' string to match 'warning' string length
+        type = 'error  ';
+        typeColor = Colors.red;
+      } else {
+        type = 'warning';
+        typeColor = Colors.yellow;
+      }
+  
+      console.log('  ',
+                  typeColor(type),
+                  Colors.gray(message.line),
+                  message.message,
+                  Colors.gray(message.ruleId));
+    }
+  }
+
+  // extra line at the end to provide visual separation between files
+  console.log();
+}
+
+/**
+ * Prints the ESLint footer
+ * @param {object} report The ESLint report to print a footer for
+ */
+function printReportFooter(report){
 
   // general report at the end
   if (report.errorCount !== 0){
@@ -64,6 +103,11 @@ function printReport(report){
   }
 }
 
+/**
+ * Updates the GitHub Status of the processed commit. This method relies on
+ *   a lot of Travis-CI and custom environment variables.
+ * @param {object} report The ESLint report
+ */
 function updateStatus(report){
 
   var github = new GitHubAPI();
